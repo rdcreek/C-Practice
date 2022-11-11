001ee3e83174a02d04e0db7676e026468f1ab2bb
Richard Creek <108111800+rdcreek@users.noreply.github.com>
2022-11-11 17:05:44 -0500

    Create SizeOfStruct.c

---
+++ Structs/SizeOfStruct.c
@@ -0,0 +1,24 @@
+/*Simple program to print out the size of a struct and its components.  This is my first time using %zu as
+a format specifier.*/
+
+
+#include <stdio.h>
+
+struct student{
+	char firstName[5];
+	char lastName[5];
+	int birthYear;
+	double aveGrade;
+};
+
+int main(void) {
+   
+	struct student me;
+    printf("Size of struct student is %zu.\n", sizeof(struct student));
+    printf("Size of firstName is %zu.\n", sizeof(me.firstName));
+    printf("Size of lastName is %zu.\n", sizeof(me.lastName));
+    printf("Size of birthYear is %zu.\n", sizeof(me.birthYear));
+    printf("Size of aveGrade is %zu.\n", sizeof(me.aveGrade));
+
+	return 0;
+}
