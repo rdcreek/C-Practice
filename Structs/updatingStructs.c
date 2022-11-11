3370300eda5bc3603213994d9281d10d1eb9ee89
Richard Creek <108111800+rdcreek@users.noreply.github.com>
2022-11-11 16:15:50 -0500

    Create updatingStructs.c

---
+++ Structs/updatingStructs.c
@@ -0,0 +1,28 @@
+/*This is just an example from an online course that I used to get comfortable updating the values in structs with the dot notation.*/
+
+#include <stdio.h>
+
+struct student{
+	char firstName[30];
+	char lastName[30];
+	int birthYear;
+	double aveGrade;
+};
+
+int main(void) {
+    struct student learner;
+    printf("First name: ");
+    scanf("%s", learner.firstName);
+    printf("Last name: ");
+    scanf("%s", learner.lastName);
+    printf("Year of birth:");
+    scanf("%d", &learner.birthYear);
+    printf("Average grade: ");
+    scanf("%lf", &learner.aveGrade);
+    
+    printf("Name: %s %s\n", learner.firstName, learner.lastName);
+	printf("Year of birth: %d\n",learner.birthYear);
+	printf("Average grade: %.2lf\n",learner.aveGrade);
+    
+	return 0;
+}
