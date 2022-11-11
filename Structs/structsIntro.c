601bf242c764138d38d7becfae4ea1f75e7e1340
Richard Creek <108111800+rdcreek@users.noreply.github.com>
2022-11-11 16:04:39 -0500

    Create structsIntro.c

---
+++ Structs/structsIntro.c
@@ -0,0 +1,20 @@
+/* Simple program to mess around with customized structs and the dot notation for them. */
+
+#include <stdio.h>
+
+struct student{
+    char firstName[30];
+    char lastName[30];
+    int birthYear;
+    double aveGrade;
+};
+
+int main(void) {
+	//! showMemory(start=65520)
+    struct student me = {"Chuck", "Norris", 1965, 3.5};
+    struct student you = {"Sean", "Connery", 1947, 3.5};
+    printf("Names: %s %s, %s %s\n", me.firstName, me.lastName, you.firstName, you.lastName);
+    printf("Year of birth: %d\n", me.birthYear);
+    printf("Average grade: %.2lf\n", me.aveGrade);
+	return 0;
+}
