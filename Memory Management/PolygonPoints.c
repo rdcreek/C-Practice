f34d3d91f20fd07c73fa590b7ff01cddf4deea64
Richard Creek <108111800+rdcreek@users.noreply.github.com>
2022-11-13 00:43:58 -0500

    Create PolygonPoints.c

---
+++ Memory Management/PolygonPoints.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct point{
+	int x;
+	int y;
+};
+
+void printPoint(struct point);
+void printPoly(struct point *, int);
+void initializePoly(struct point *, int);
+
+int main(void) {
+    
+    // Fill in your main function here
+int num;
+
+struct point *polygon;
+
+scanf("%d", &num);
+
+polygon = (struct point *) malloc(num * sizeof(struct point));
+
+initializePoly(polygon, num);
+printPoly(polygon, num); 
+free(polygon); 
+}
+
+void printPoint(struct point pt) {
+    printf("(%d, %d)\n", pt.x, pt.y);
+}
+
+void printPoly(struct point *ptr, int N) {
+    int i;
+    for (i=0; i<N; i++) {
+        printPoint(ptr[i]);
+    }
+}
+
+// Write your initializePoly() function here
+void initializePoly(struct point * ptr, int n){
+    for(int i = 0; i < n; i++){
+        ptr[i].x = 0 - i;
+        ptr[i].y = i * i; 
+    }
+}
