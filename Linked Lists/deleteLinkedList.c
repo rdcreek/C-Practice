e974e5e76cb083142458b8acb52c0e0abd52fdfa
Richard Creek <108111800+rdcreek@users.noreply.github.com>
2022-11-13 01:43:08 -0500

    Create deleteLinkedList.c

---
+++ Linked Lists/deleteLinkedList.c
@@ -0,0 +1,77 @@
+/* There is a lot going on in this one.  I saved it here so that I could go back and review it later.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct point{
+    int x;
+    int y;
+    struct point * next;
+};
+
+\\Function Prototypes
+void printPoints(struct point *start);
+struct point * createPoint(int x, int y) ;
+struct point * append (struct point * end, struct point * newpt);
+void freePoints(struct point * start);
+
+
+int main(void) {
+   
+    struct point * start, * end, * newpt;
+    int num, i;
+    int x, y;
+    
+    printf("How many points? ");
+    scanf("%d", &num);
+    for (i=0; i<num; i++) {
+        printf("x = ");
+        scanf("%d", &x);
+        printf("y = ");
+        scanf("%d", &y);
+        newpt = createPoint(x,y);
+        if (i==0) {
+            start = end = newpt;
+        } else {
+            end = append(end,newpt);
+        }
+    }
+    printf("You entered: ");
+    printPoints(start);
+    freePoints(start);
+	return 0;
+}
+
+void printPoints(struct point *start) {
+   
+    struct point * ptr;
+    ptr = start;
+    while (ptr!=NULL) {
+        printf("(%d, %d)\n", ptr->x, ptr->y);
+        ptr = ptr->next;
+    }
+}
+
+struct point * append (struct point * end, struct point * newpt) {
+    end->next = newpt;
+    return(end->next);
+} 
+
+struct point * createPoint(int x, int y) {
+    struct point *ptr;
+    ptr = (struct point *)malloc(sizeof(struct point));
+    ptr->x = x;
+    ptr->y = y;
+    ptr->next = NULL;
+    return(ptr);
+}
+
+void freePoints(struct point * start) {
+    struct point * ptr = start;
+    while (ptr!=NULL) {
+        start = ptr;
+        ptr = ptr->next;
+        free(start);
+    }
+}
