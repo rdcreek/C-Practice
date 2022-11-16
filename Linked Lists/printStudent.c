fb3ed32536c52cb23b42ca4dbd8af0b9522bf536
Richard Creek <108111800+rdcreek@users.noreply.github.com>
2022-11-16 18:46:14 -0500

    Create printStudent.c

---
+++ Linked Lists/printStudent.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct student {
+      char name[50];
+      int age;
+      struct student *next;
+};
+
+struct student *createStudent(char studentName[50], int studentAge);
+struct student * append(struct student * end, struct student * newStudptr); 
+void printStudents(struct student *start);
+/* add other prototypes here if needed*/
+
+int main(void) {
+    struct student *start, *newStudptr, *end, *tmp;
+    int ageP, ageR, ageM;
+
+    scanf("%d %d %d", &ageP, &ageR, &ageM);
+
+    start = createStudent("Petra", ageP);
+    end = start;
+
+    newStudptr = createStudent("Remi", ageR);
+    end = append(end, newStudptr);
+
+    newStudptr = createStudent("Mike", ageM);
+    end = append(end, newStudptr);
+
+    printStudents(start);
+    tmp = start->next;
+
+    free(start);
+
+    start = tmp;
+    tmp = start->next;
+
+    free(start);
+    free(tmp);
+
+    return 0;
+}
+
+/* Place your function definitions here. Be sure to include the definitions for 
+   createStudent() and append() as well as any other functions you created for 
+   the previous tasks. */
+   
+   struct student *createStudent(char studentName[], int studentAge){
+    struct student* ptr;
+    ptr = (struct student*)malloc(sizeof(struct student));
+    //ptr->name = studentName;
+    strcpy(ptr->name, studentName);
+    ptr->age = studentAge; 
+    ptr->next = NULL; 
+    
+    return ptr; 
+}
+
+struct student *append(struct student * end, struct student * newStudptr){
+    
+    end->next = newStudptr;
+    end = newStudptr;
+    return(end);
+}
+
+void printStudents(struct student *start){
+    struct student * ptr = start;
+    while (ptr!=NULL) {
+        printf("%s is %d years old.\n", ptr->name, ptr->age);
+        ptr = ptr->next;
+    }
+}
