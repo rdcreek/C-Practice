081b66a7bb8da358b3ec81d9d1ac8518b1de94e6
Richard Creek <108111800+rdcreek@users.noreply.github.com>
2022-11-16 01:02:52 -0500

    Create studentNode.c

---
+++ Linked Lists/studentNode.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct student {
+      char name[50];
+      int age;
+      struct student *next;
+};
+
+struct student *createStudent(char studentName[], int studentAge);
+
+// Write other function prototypes here (if any)
+void copyStr(char* source, char* target);
+
+int main(void) {
+    struct student *studptr;
+    int myAge;
+    char myName[50];
+    scanf("%s %d", myName, &myAge);
+    studptr = createStudent(myName, myAge);
+    printf("New student created: %s is %d years old.\n", studptr->name, studptr->age);
+    free(studptr);
+    return 0;
+}
+
+// Write your createStudent function here (and any other functions you see fit)
+struct student *createStudent(char studentName[], int studentAge){
+    struct student* ptr;
+    ptr = (struct student*)malloc(sizeof(struct student));
+    //ptr->name = studentName;
+    strcpy(ptr->name, studentName);
+    ptr->age = studentAge; 
+    ptr->next = NULL; 
+    
+    return ptr; 
+}
