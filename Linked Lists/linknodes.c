16f356f27fb61234e451fd4256b47a2bce0914dc
Richard Creek <108111800+rdcreek@users.noreply.github.com>
2022-11-16 22:31:31 -0500

    Update linknodes.c
    The program now creates a linked list from characters entered by the user.

--- Linked Lists/linknodes.c
+++ Linked Lists/linknodes.c
@@ -11,26 +11,15 @@ struct digit * createDigit(int dig);
 struct digit * append(struct digit * end, struct digit * newDigitptr);
 void printNumber(struct digit *start);
 void freeNumber(struct digit *start); 
-    
+struct digit * readNumber();    
     
 int main(void) {
    
-    struct digit *start, *newDigitptr, *end, *tmp;
-    int first = 5;
-    int second = 3;
-    int third = 7;
-    int fourth = 9; 
-    start = createDigit(first);
-    end = start;
-    newDigitptr = createDigit(second);
-    end = append(end, newDigitptr);
-    newDigitptr = createDigit(third);
-    end = append(end, newDigitptr);
-    newDigitptr = createDigit(fourth);
-    end = append(end, newDigitptr);
-    printNumber(start); 
+    struct digit *start;
+    printf("Please enter a number: ");
+    start = readNumber();
+    printNumber(start);
     freeNumber(start);
-   
     return 0;
 }
 
@@ -67,4 +56,24 @@ void freeNumber(struct digit *start) {
         free(ptr);
         ptr = tmp;
     }
+}
+
+struct digit * readNumber() {
+    char c;
+    int d;
+    struct digit *start, *end, *newptr;
+    start = NULL;
+    scanf("%c", &c);
+    while (c!='\n') {
+        d = c - 48;
+        newptr = createDigit(d);
+        if (start==NULL) {
+            start = newptr;
+            end = start;
+        } else {
+            end = append(end, newptr);
+        }
+        scanf("%c", &c);
+    }
+    return start;
 }
